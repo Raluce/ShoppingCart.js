1d526fa8c0add6e5f591a932d0acbb2c82565870
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-01-12 21:23:21 -0500

    Added add method to append product to a store's shopping cart

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,18 +1,20 @@
 ShoppingCart = function() {  
   /**
-   * Adds a product to the shopping cart
+   * Adds a product to store's shopping cart
    * @param {String} storeId Unique identifier to current store
-   * @param {Object} product Item being placed in the shopping cart
+   * @param {Object} product Item to add to cart
    */
   var add = function(storeId, product) {
-    sessionStorage.set
+    var productList = get(storeId);
+    productList.push(product);
+    sessionStorage.setItem(storeId, JSON.stringify(productList));
   };
-  
+
   var remove = function(product) {};
   
   /**
    * Get all products in store's shopping cart
-   * @param {*} storeId Unique identifier to current store
+   * @param {String} storeId Unique identifier to current store
    */
   var get = function(storeId) {
     var productString = sessionStorage.getItem(storeId);
