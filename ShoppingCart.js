6665228ef6ba97e75f07f20b9af8a7437f8f43a3
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-01-12 21:28:26 -0500

    Added remove product from a store's shopping cart

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -10,7 +10,18 @@ ShoppingCart = function() {
     sessionStorage.setItem(storeId, JSON.stringify(productList));
   };
 
-  var remove = function(product) {};
+  /**
+   * Removes first instance of product in store's shopping cart
+   * @param {Object} product Item to remvoe from cart
+   */
+  var remove = function(storeId, product) {
+    var productList = get(storeId);
+    var productIndex = productList.indexOf(product);
+
+    if(productIndex !== -1) {
+      productList.slice(productIndex, 1);
+    }
+  };
   
   /**
    * Get all products in store's shopping cart
@@ -30,6 +41,6 @@ ShoppingCart = function() {
   return {
     add: add,
     remove: remove,
-    get: get
+    get: get,
   };
 }();
