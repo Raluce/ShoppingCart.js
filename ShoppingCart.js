18e09d9f1f15adc33e2df21fb7fd5aebdfa153a1
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-01-12 21:16:08 -0500

    Added get() functionality

--- ShoppingCart.js
+++ ShoppingCart.js
@@ -1,7 +1,29 @@
-ShoppingCart = function() {
-  var add = function(storeId, product) {};
+ShoppingCart = function() {  
+  /**
+   * Adds a product to the shopping cart
+   * @param {String} storeId Unique identifier to current store
+   * @param {Object} product Item being placed in the shopping cart
+   */
+  var add = function(storeId, product) {
+    sessionStorage.set
+  };
+  
   var remove = function(product) {};
-  var get = function() {};
+  
+  /**
+   * Get all products in store's shopping cart
+   * @param {*} storeId Unique identifier to current store
+   */
+  var get = function(storeId) {
+    var productString = sessionStorage.getItem(storeId);
+    
+    try {
+      var productList = JSON.parse(productString);
+      return productList;
+    } catch(e) {
+      return [];
+    }
+  };
 
   return {
     add: add,
