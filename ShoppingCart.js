c3584e10dd0fa9f96b0ec091757cf6e3b2f14d55
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-01-12 20:56:55 -0500

    Added basic methods. Need to be implemented

---
+++ ShoppingCart.js
@@ -0,0 +1,11 @@
+ShoppingCart = function() {
+  var add = function(storeId, product) {};
+  var remove = function(product) {};
+  var get = function() {};
+
+  return {
+    add: add,
+    remove: remove,
+    get: get
+  };
+}();
