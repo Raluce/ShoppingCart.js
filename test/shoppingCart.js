2f59f2d518448925eed7efd656c3ed4480f4e683
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-01-31 10:59:42 -0500

    Added test for getting all products from shopping cart

--- test/shoppingCart.js
+++ test/shoppingCart.js
@@ -16,8 +16,19 @@ describe('ShoppingCart', () => {
 
   it('Should add product to shopping cart', () => {
     ShoppingCart.add('store1', product);
+    ShoppingCart.add('store2', []);
     const result = ShoppingCart.get('store1');
     
     result.should.be.instanceof(Array).and.have.lengthOf(1);
+  });
+
+  it('Should get all products from shopping cart', () => {
+    const result = ShoppingCart.get('store1');
+
+    result.should.be.instanceof(Array).and.have.lengthOf(1);
+    result[0].id.should.equal(product.id);
+    result[0].name.should.equal(product.name);
+    result[0].price.currency.should.equal(product.price.currency);
+    result[0].price.cost.should.equal(product.price.cost);
   });
 });
