d20ec0d0c977257194de1d559868fd3fcb2c7139
Camilo Gonzalez <cgs.camilogonzalez@gmail.com>
2018-03-23 23:36:56 -0400

    create product group works correctly

---
+++ Product.js
@@ -0,0 +1,12 @@
+function Product(product) {
+  if (!product.id) throw new Error('product[id] is required');
+  
+  this.id = product.id;
+  this.name = product.name ? product.name : '';
+  this.price = {
+    cost: product.price.cost ? product.price.cost : 0,
+    tax: product.price.tax ? product.price.tax : 0,
+    currency: product.price.currency ? product.price.currency : 'usd'
+  },
+  this.properties = product.properties;
+}
---
+++ test/ProductGroup.spec.js
@@ -0,0 +1,33 @@
+const should = require('should');
+require('./../ProductGroup');
+
+describe('Product Group', () => {
+  var productGroup = null;
+  const product = {
+    id: '1a',
+    name: 'chicken',
+    properties: {
+      description: 'best chicken ever'
+    },
+    price: {
+      cost: 9.99,
+      tax: 0.15,
+      currency: 'usd'
+    }
+  };
+
+  beforeEach(() => {
+    productGroup = new ProductGroup(product);
+  });
+
+  it('Should create a product group', () => {
+    productGroup.count.should.equal(1);
+    
+    productGroup.product.id.should.equal(product.id);
+    productGroup.product.name.should.equal(product.name);
+    productGroup.product.price.cost.should.equal(product.price.cost);
+    productGroup.product.price.tax.should.equal(product.price.tax);
+    productGroup.product.price.currency.should.equal(product.price.currency);
+    productGroup.product.properties.description.should.equal(product.properties.description);
+  });
+});
